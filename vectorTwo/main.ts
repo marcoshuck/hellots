e79f38dbbfa12d897a5b974eef33a29b98178352
Marcos Huck <marcos.h3012@gmail.com>
2019-08-10 01:51:02 -0300

    Add 2D vector class

---
+++ vectorTwo/main.ts
@@ -0,0 +1,10 @@
+import { Vector2 } from "./vector2";
+
+const vectorA: Vector2 = new Vector2(5, 2);
+const vectorB: Vector2 = new Vector2(-3, 4);
+
+console.log('Vector A', vectorA);
+
+console.log('Vector B', vectorB);
+
+console.log('A.B =', vectorA.dot(vectorB));
